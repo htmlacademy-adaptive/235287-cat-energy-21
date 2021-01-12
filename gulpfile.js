858473b8ef69ec937d443c580b1afb21a559c601
RomanTkachev <not.so.bad@yandex.ru>
2021-01-13 01:10:32 +0300

    сделал часть автоматизации до js

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,13 @@ const sourcemap = require("gulp-sourcemaps");
 const sass = require("gulp-sass");
 const postcss = require("gulp-postcss");
 const autoprefixer = require("autoprefixer");
+const csso = require("postcss-csso"); //установить npm i -D postcss-csso
 const sync = require("browser-sync").create();
+const imagemin = require("gulp-imagemin");
+const webp = require("gulp-webp"); //установить npm i -D gulp-webp
+const svgstore = require("gulp-svgstore") //установить npm i -D gulp-svgstore
+const rename = require("gulp-rename"); //установить npm i -D gulp-rename
+const htmlmin = require("gulp-htmlmin"); // установить npm i -D gulp-htmlmin
 
 // Styles
 
@@ -14,21 +20,66 @@ const styles = () => {
     .pipe(sourcemap.init())
     .pipe(sass())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
     .pipe(sourcemap.write("."))
-    .pipe(gulp.dest("source/css"))
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("dist/css"))
     .pipe(sync.stream());
 }
 
 exports.styles = styles;
+
+//HTML
+
+const html = () => {
+  return gulp.src("source/*.html")
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest("dist"))
+
+}
+
+
+exports.html = html;
+//Images
+
+const images = () => {
+  return gulp.src("source/img/**/*{jpg,png,svg}")
+    .pipe(imagemin([
+      imagemin.mozjpeg({progressive: true}),
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.svgo()
+    ]))
+    .pipe(gulp.dest("dist/img"))
+}
+
+exports.images = images;
+
+//WebP
+
+const createWebp = () => {
+  return gulp.src("source/img/**/*{jpg,png}")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("dist/img"))
+}
+
+exports.createWebp = createWebp;
+
+// Sprite
+
+const sprite = () => {
+  return gulp.src("source/img/icons/*.svg")
+    .pipe(svgstore())
+    .pipe(gulp.dest("dist/img"))
+}
 
 // Server
 
 const server = (done) => {
   sync.init({
     server: {
-      baseDir: 'source'
+      baseDir: 'dist'
     },
     cors: true,
     notify: false,
@@ -45,6 +96,18 @@ const watcher = () => {
   gulp.watch("source/sass/**/*.scss", gulp.series("styles"));
   gulp.watch("source/*.html").on("change", sync.reload);
 }
+
+//Build
+
+const build = gulp.parallel(
+  styles,
+  html,
+  sprite,
+  images,
+  createWebp
+)
+
+exports.build = build;
 
 exports.default = gulp.series(
   styles, server, watcher
