0ae97c011bf420710e6bb79ad4e2c651c3497c90
RomanTkachev <not.so.bad@yandex.ru>
2021-01-17 01:46:38 +0300

    исправляет внешний вид слайдера: коты накладываются друг на друга

--- source/js/scripts.js
+++ source/js/scripts.js
@@ -16,29 +16,6 @@ navToggle.addEventListener('click', function() {
   }
 });
 
-
-//slider
-
-
-var sliderItemBefore = document.querySelector('.slider__item--fat');
-var sliderItemAfter = document.querySelector('.slider__item--thin');
-var sliderButtonBefore = document.querySelector('.slider__controls--before');
-var sliderButtonAfter = document.querySelector('.slider__controls--after');
-
-sliderButtonBefore.addEventListener('click', function () {
-  if (sliderItemAfter.classList.contains('slider__item--current')) {
-    sliderItemAfter.classList.remove('slider__item--current')
-    sliderItemBefore.classList.add('slider__item--current');
-  }
-});
-
-sliderButtonAfter.addEventListener('click', function () {
-  if (sliderItemBefore.classList.contains('slider__item--current')) {
-    sliderItemBefore.classList.remove('slider__item--current')
-    sliderItemAfter.classList.add('slider__item--current');
-  }
-});
-
 //Catalog-button
 
 var buttonMore = document.querySelector('.button--grey');
